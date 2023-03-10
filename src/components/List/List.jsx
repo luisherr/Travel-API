f64e26363a0173a1ecd5782a537c3ca2a0e17123
Luis Herrera <luis378962@gmail.com>
2023-03-10 14:26:34 -0600

    Inicio del proyecto

---
+++ src/components/List/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+const List = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default List
